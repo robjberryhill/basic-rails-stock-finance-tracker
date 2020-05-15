464220cde9786fcb137899363c2d0844eb1aa79a
robjberryhill <robjberryhill@gmail.com>
2020-05-14 20:20:10 -0700

    Final Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
-  resources :users, only: [:show]
+
   devise_for :users
 
   devise_scope :user do
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   get "my_portfolio", to: "users#my_portfolio"
   get "search_friends", to: "users#search_friends"
   get "search_stock", to: "stocks#search"
+  resources :users, only: [:show]
 end
